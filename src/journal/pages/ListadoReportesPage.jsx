92c863bdd9b7c29a590d1aaedfd3f9f0bd8e9c23
Jorge Cuevas <20152704@itla.edu.do>
2022-08-17 01:23:19 -0400

    Adding DataGrid Component

--- src/journal/pages/ListadoReportesPage.jsx
+++ src/journal/pages/ListadoReportesPage.jsx
@@ -6,7 +6,8 @@ import 'moment/dist/locale/es-do';
 moment.locale('es-do')
 
 import { AddOutlined } from "@mui/icons-material"
-import { IconButton, List, ListItem, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
+import { Box, IconButton, List, ListItem, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
+import { DataGrid } from '@mui/x-data-grid';
 
 import { UserLayout } from "../layout/UserLayout"
 import { AdminLayout } from "../layout/AdminLayout"
@@ -19,7 +20,28 @@ export const ListadoReportesPage = () => {
   const dispatch = useDispatch();
   const {isSaving, active } = useSelector( state => state.journal);
 
-  const {notes} = useSelector(state => state.journal);
+  const {notes: notesData} = useSelector(state => state.journal);
+
+        //Status 0: Sin Guardar
+        //Status 1: Pendiente de Cambios
+        //Status 2: Pendiente de aprobacion
+        //Status 3: Rechazada
+        //Status 4: Aprobada
+        //Status 5: Resuelta
+        //Status 6: Expirada
+
+  const notes =  notesData.map((row) => {
+    return {...row, 
+      date: moment(row.date).format('LL'), 
+      status: (row.status === 0) ? 'Sin Guardar' :
+              (row.status === 1) ? 'Pendiente de Cambios' :
+              (row.status === 2) ? 'Pendiente de aprobacion' :
+              (row.status === 3) ? 'Rechazada' :
+              (row.status === 4) ? 'Aprobada' :
+              (row.status === 5) ? 'Resuelta' :
+              'Expirada' 
+    }
+  })
 
   const onClickNewNote = () => {
     dispatch(startNewNote());
@@ -30,40 +52,24 @@ export const ListadoReportesPage = () => {
       
       <Typography variant="h5" sx={{mb: 1}} color='primary'>Listado de Reportes</Typography>
 
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Nombre</TableCell>
-            <TableCell align="right">Provincia</TableCell>
-            <TableCell align="right">Municipio</TableCell>
-            <TableCell align="right">Raza</TableCell>
-            <TableCell align="right">Sexo</TableCell>
-            <TableCell align="right">Fecha</TableCell>
-            <TableCell align="right">Estatus</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {notes.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.title}
-              </TableCell>
-              <TableCell align="right">{row.provincia}</TableCell>
-              <TableCell align="right">{row.municipio}</TableCell>
-              <TableCell align="right">{row.raza}</TableCell>
-              <TableCell align="right">{row.sexo}</TableCell>
-              <TableCell align="right">{moment(row.date).format('LL')}</TableCell>
-              <TableCell align="right">{row.status}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-
+      <Box style={{ width: '100%' }}>
+      <DataGrid
+        autoHeight
+        rows={notes}
+        columns={[
+          { field: 'title', headerName: 'Nombre', flex: 1 },
+          { field: 'provincia', headerName: 'Provincia', flex: 1},
+          { field: 'municipio', headerName: 'Municipio', flex: 1},
+          { field: 'raza', headerName: 'Raza', flex: 1},
+          { field: 'sexo', headerName: 'Sexo', flex: 1},
+          { field: 'date', headerName: 'Fecha', flex: 1},
+          { field: 'status', headerName: 'Estatus', flex: 1},
+        ]
+        }
+        pageSize={10}
+        rowsPerPageOptions={[10]}        
+      />
+    </Box>
     </AdminLayout>
   )
 }
